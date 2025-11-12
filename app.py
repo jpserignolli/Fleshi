48005585b3b16e3fd3c215bf1cd476101bd95c26
jpserignolli <jpserig@gmail.com>
2025-11-12 14:43:52 -0300

    feat: add homepage and profile pages

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Bem-vindo a Fleshi!"
+    return render_template('homepage.html')
+
+@app.route('/profile/<username>')
+def profile(username):
+    return render_template('perfil.html', username=username)
 
 if __name__ == '__main__':
     app.run(debug=True)
