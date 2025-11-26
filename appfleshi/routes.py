d2d0b97e4737f0b1366fd5ef83cf26c97279e37a
jpserignolli <jpserig@gmail.com>
2025-11-26 14:06:20 -0300

    feat: add loginForm and registerForm in pages

--- appfleshi/routes.py
+++ appfleshi/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import Flask, render_template, request
 from flask_login import login_required
 from appfleshi import app
 from appfleshi.forms import LoginForm, RegisterForm
